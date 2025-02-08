c7bd93e0973ab56b7e7d1aad74e98dbb324186cc
Yujie <ujiewu@gmail.com>
2025-02-08 02:22:41 -0800

    only doing this so i can program on my pi. added communications.py pt 2 i forgot to save

--- communications.py
+++ communications.py
@@ -1,13 +1,24 @@
+import subprocess
+
 def main():
     print("Hello, World!")
     ground_station_address = "C4:91:0C:A7:EA:EF" #Yujie's mac address
     file_path = ""
     current_RSSI = 0
+    RSSI_threshold = -70
     # TODO: subprocess to connect to pi via bluetooth
     # for loop: send a file (placeholder for now) to pi w/ btmgmt and record RSSI
     # if RSSI at a threshold, send a file to the pi via obexftp
     # 
+    try:
+        while True:
+            #sudo btmgmt find | grep "C4:91:0C:A7:EA:EF" 
+            something = subprocess.Popen(["sudo", "btmgmt",  "|", "grep", ground_station_address], stdout=subprocess.PIPE, text=True)
+            print(something.stdout.read())
+            pass
+    except KeyboardInterrupt:
+        print("Exiting...")
 
 
-if __name__== "__main__":
+if __name__== "__main__": 
     main()
