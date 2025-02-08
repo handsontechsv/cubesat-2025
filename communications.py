a44bc38bf05d0f7d45fa788d042db91cdaa6fff7
Yujie <ujiewu@gmail.com>
2025-02-08 02:18:36 -0800

    only doing this so i can program on my pi. added communications.py

---
+++ communications.py
@@ -0,0 +1,13 @@
+def main():
+    print("Hello, World!")
+    ground_station_address = "C4:91:0C:A7:EA:EF" #Yujie's mac address
+    file_path = ""
+    current_RSSI = 0
+    # TODO: subprocess to connect to pi via bluetooth
+    # for loop: send a file (placeholder for now) to pi w/ btmgmt and record RSSI
+    # if RSSI at a threshold, send a file to the pi via obexftp
+    # 
+
+
+if __name__== "__main__":
+    main()
