14eebce0e131551993f53f6d888c6e7b05fb7caf
gabriel-08 <57821142+gabriel-08@users.noreply.github.com>
2025-02-20 15:10:53 -0800

    database

--- database/dbfunctions.py
+++ database/dbfunctions.py
@@ -4,7 +4,7 @@ import sqlite3
 def create():
     conn = sqlite3.connect('coords.db')
     cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE coordinates(
+    cursor.execute('''CREATE TABLE IF NOT EXISTS coordinates(
     Top_Left_Lat REAL,
     Top_Left_long REAL,
     Brightness REAL,
@@ -17,22 +17,26 @@ def create():
     Second INTEGER); ''')
 
 
-def write(data):
+def write(data: list):
     conn = sqlite3.connect('coords.db')
     cursor = conn.cursor()
-    val = str(data[0]) + ", " + str(data[1]) + ",` " + str(data[2]) + ", " + str(data[3]) + ", " + str(data[4]) + \
+    val = str(data[0]) + ", " + str(data[1]) + ", " + str(data[2]) + ", " + str(data[3]) + ", " + str(data[4]) + \
         ", " + str(data[5]) + ", " + str(data[6]) + ", " + str(data[7]) + ", " + str(data[8]) + ", " + str(data[9])
     cursor.execute(f'''INSERT INTO coordinates VALUES({val}); ''')
     conn.commit()
 
 
-def get_filter(tllat, tllong):
+def get_filter(lat_long: str):
     conn = sqlite3.connect("coords.db")
     cursor = conn.cursor()
-    rows = cursor.execute('''SELECT * FROM coordinates WHERE Top_Left_Lat = ''' + str(tllat) +  ''' AND Top_Left_Long 
-    = ''' + str(tllong))
+    # ind = lat_long.find(" ")
+    lat = lat_long.split(" ")[0]
+    long = lat_long.split(" ")[1]
+    rows = cursor.execute('''SELECT * FROM coordinates WHERE Top_Left_Lat = ''' + lat +  ''' AND Top_Left_Long = ''' + long)
+    ret = []
     for i in rows:
-        print(i)
+        ret.append(i)
+    return ret
 
 
 def get():
@@ -42,3 +46,7 @@ def get():
     for i in rows:
         print(i)
     conn.close()
+
+
+def getID(lat, long):
+    return lat + " " + long
